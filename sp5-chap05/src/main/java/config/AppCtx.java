22cc887162d7838a4f780df09b98eb99bfd666d0
BroadLeopard <whtngur155@gmail.com>
2022-05-09 20:13:06 +0900

    chapter 5 end

---
+++ sp5-chap05/src/main/java/config/AppCtx.java
@@ -0,0 +1,35 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import spring.MemberPrinter;
+import spring.MemberSummaryPrinter;
+import spring.VersionPrinter;
+
+@Configuration
+@ComponentScan(basePackages = {"spring"})//spring 패키지에서 component 어노테이션 붙으면 스프링 빈 객체에 자동으로 등록한다.
+public class AppCtx {
+
+	@Bean
+	@Qualifier("printer")
+	public MemberPrinter memberPrinter1() {
+		return new MemberPrinter();
+	}
+	
+	@Bean
+	@Qualifier("summaryPrinter")
+	public MemberSummaryPrinter memberPrinter2() {
+		return new MemberSummaryPrinter();
+	}
+	
+	@Bean
+	public VersionPrinter versionPrinter() {
+		VersionPrinter versionPrinter = new VersionPrinter();
+		versionPrinter.setMajorVersion(5);
+		versionPrinter.setMinorVersion(0);
+		return versionPrinter;
+	}
+}
